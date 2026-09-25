3361a9abb0ebea81f7b743ec76b8cfd01d0a0428
Corey Shih <46582009+CoreyShih@users.noreply.github.com>
2026-09-25 12:17:10 -0700

    Complete foundations/loops_and_arrays

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!(Number.isInteger(num1) && num1 > 0) ||
+        !(Number.isInteger(num2) && num2 > 0)) {
+            return "ERROR";
+        }
+    let result = 0;
+        for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+            result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
